ce20a754f6570c9cb066a07e48d763d6ef805273
firefly017 <jui.n.nahar@gmail.com>
2020-05-06 20:42:32 +0200

    starting the doctor schedule page

--- src/pages/DoctorSchedule.js
+++ src/pages/DoctorSchedule.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function DoctorSchedule() {
-  return <div>ok great</div>;
+  return (
+    <div>
+      <h1>Who is on duty?</h1>
+      <p>
+        We are : <b>open</b>
+      </p>
+      <p>To make an appointment</p>
+      <p>call: 020 555 5555</p>
+    </div>
+  );
 }
