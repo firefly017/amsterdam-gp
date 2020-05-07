8eefda6b343164e556389a6d5c3a661a418e6003
firefly017 <jui.n.nahar@gmail.com>
2020-05-07 18:06:48 +0200

    creating signup forms

--- src/pages/PatientDatabase.js
+++ src/pages/PatientDatabase.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function PatientDatabase() {
-  return <div>one more page</div>;
+  return (
+    <div>
+      <h1>Patient Database </h1>
+    </div>
+  );
 }
--- src/pages/PatientSignUp.js
+++ src/pages/PatientSignUp.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function PatientSignUp() {
-  return <div>signup here</div>;
+  // const [name, set_name] = useState("");
+  return (
+    <div className="PatientSignUpForm">
+      <h1>Patient Sing up</h1>
+      <p>
+        First name : <input type="text" placeholder="First Name" />
+      </p>
+      <p>
+        Last name : <input type="text" placeholder="Last name" />
+      </p>
+      <p>
+        E-mail : <input type="text" placeholder="E-mail" />
+      </p>
+      <p>
+        Phone : <input type="text" placeholder="Phone number" />
+      </p>
+      <p>
+        <label for="gender">Gender : </label>
+        <select id="gender">
+          <option value="select">Select Gender</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+          <option value="other">Other</option>
+        </select>
+      </p>
+      <p>
+        <label for="date of birth">
+          Date of birth :{" "}
+          <input type="date" id="date of birth" name="date of birth"></input>
+        </label>
+      </p>
+
+      <p>
+        <button type="button">Submit</button>
+      </p>
+    </div>
+  );
 }
