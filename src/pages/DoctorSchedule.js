11a0213dbf4d4f0edb71c4377aceed438a0334e3
firefly017 <jui.n.nahar@gmail.com>
2020-05-07 10:19:57 +0200

    who is on duty

--- src/pages/DoctorSchedule.js
+++ src/pages/DoctorSchedule.js
@@ -4,6 +4,7 @@ import MakeAppoinment from "../components/MakeAppoinment";
 export default function DoctorSchedule() {
   return (
     <div>
+      <h1>Who is on duty?</h1>
       <MakeAppoinment />
     </div>
   );
